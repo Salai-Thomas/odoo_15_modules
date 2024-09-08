31a8d7f02b2ee96ac17ac4e4e1763c643ccdcdd4
SalaiThomas <salaithomas582@gmail.com>
2024-09-08 09:16:55 +0700

    added_status

--- to_do_list/models/todo_item.py
+++ to_do_list/models/todo_item.py
@@ -4,5 +4,13 @@ class TodoItem(models.Model):
     _name = 'todo.item'
 
     name = fields.Char(string='Name')
-    is_done = fields.Boolean(string='Is Done?')
+    is_done = fields.Boolean()
+    label_done = fields.Char(string='Status',compute="_compute_dynamic_label",readonly=1)
     work_team_ids = fields.Many2many('res.partner',string='Work Team')
+
+    def _compute_dynamic_label(self):
+        for record in self:
+            if record.is_done:
+                record.label_done = 'Done'
+            else:
+                record.label_done = 'Not Done'
